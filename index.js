6e1e3f47c5312d7a62f12b207b043cbefe057177
Arichy <452609128@qq.com>
2019-06-13 19:09:38 +0800

    增加了webpack配置

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-var path = require('path')
-
-module.exports = function (api) {
-    api.cache.never()
-
-    var presets = ['@babel/preset-env']
-    var plugins = [
-        // ['module-resolver', {
-        //     'alias': {
-        //         '@src': path.resolve(__dirname, './src')
-        //     }
-        // }],
-        ['@babel/plugin-proposal-decorators', {
-            // decoratorsBeforeExport: true
-            legacy: true
-        }],
-        ['@babel/plugin-proposal-class-properties', {
-            loose: true
-        }],
-        '@babel/plugin-proposal-export-default-from',
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-function-bind'
-    ]
-
-    return {
-        presets: presets,
-        plugins: plugins
-    }
+module.exports = {
+    babel: {
+        presets: require('./presets'),
+        plugins: require('./plugins')
+    },
+    webpack: require('./webpack.config')
 }
